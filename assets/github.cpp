fd28199f74e25db144e3ac4837652e2204658d48
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2022-11-12 12:48:40 +0200

    cosmetics

--- assets/github.cpp
+++ assets/github.cpp
@@ -2,24 +2,25 @@
  * @file: github.cpp
  * @author: Dmitry Shpakov
  * @date: 2021-10-29
+ * @licence: CC0
  *
  */
 
-//FIXME I have no idea what this piece of code does
+// FIXME I have no idea what this piece of code does
 
 #include <github.h>
 #include <memory>
 #include <iostream>
 
 int main(int argc, char** argv) {
-    std::unique_ptr<TVisitor> visitor(GIT_REPO_VISITOR);
+    std::unique_ptr<TVisitor> visitor = new TVisitor(GIT_REPO_VISITOR);
 
     std::cout << "Welcome dear " << visitor->getName() << "!" << std::endl;
     
     return 0;
 }
 
-//XXX Compile with care and -Wall -Werror flags.
+// XXX Compile with care and -Wall -Werror flags.
 
 // vim: syntax=cpp ts=4 sw=4 sts=4 sr et
 
